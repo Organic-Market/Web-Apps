5e82efb5917abf3021e9d6f555d0fed948a880ee
Jhayro Jhordan Roncal Tacilla <jhayro2017@gmail.com>
2022-09-21 17:28:02 -0500

    Primera version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component:  HomeComponent },
+  { path: 'login', component:  LoginComponent },
+  { path: 'register', component:  RegisterComponent },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
