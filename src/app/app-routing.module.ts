c2d5c77eb3f12707cf86918323d00164a6bd5459
Jhayro Jhordan Roncal Tacilla <jhayro2017@gmail.com>
2022-09-22 19:03:21 -0500

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +9,8 @@ const routes: Routes = [
   { path: 'home', component:  HomeComponent },
   { path: 'login', component:  LoginComponent },
   { path: 'register', component:  RegisterComponent },
+  { path: 'dashboard', component:  DashboardComponent },
+
   
 
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
+import { environment } from './../../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +13,46 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   hide = true;
-  constructor() {}
+  basePath:string=environment.basePath;
+
+  public loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http:HttpClient,
+    private router:Router,
+    private snackBar: MatSnackBar,
+
+    ){}
   ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+      email:[''],
+      password:['']
+    })
   }
 
+  login(){
+    this.http.get<any>(this.basePath)
+    .subscribe(res=>{
+      const user=res.find((a:any)=>{
+        return a.email===this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user){
+        this.snackBar.open('Ingresaste', '', {
+          duration: 3000,
+        });
+        this.loginForm.reset();
+        this.router.navigate(['dashboard'])
+      }else{
+        this.snackBar.open('El usuario es Incorrecto', '', {
+          duration: 3000,
+        });
+      }
+    },err=>{
+      this.snackBar.open('Algo salio Mal', '', {
+        duration: 3000,
+      });
+    })
+
+  }
 }
