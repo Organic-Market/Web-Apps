ebea5d465714ed60ecf069e9633c5349419cd34d
Luis Broncano <luiso2.2001b@gmail.com>
2022-11-06 03:25:29 -0500

    Ultimos cambios

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -29,12 +29,11 @@ export class ShopComponent implements OnInit {
     });
   }
 
-  saveProduct(id:any,unit_price:any,name:any,stock:any,category:any): void {
+  saveProduct(id:any,unit_price:any,name:any,stock:any): void {
     const product: Product = {
       id: id,
       name: name,
       unit_price: unit_price,
-      category: category,
       stock: stock,
 
     };
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,5 @@ export interface Product {
     id: number;
     name: string;
     unit_price: number;
-    category: any;
     stock: number;
   }
